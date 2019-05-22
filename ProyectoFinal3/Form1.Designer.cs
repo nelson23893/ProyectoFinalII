eea1bd922805ce381c55be8c2f6558f44c0ba5dd
Ramirez <Ramirez@DESKTOP-5BR9HMJ>
2019-05-21 23:29:08 -0600

    version2

--- ProyectoFinal3/Form1.Designer.cs
+++ ProyectoFinal3/Form1.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.ckbAudio = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(261, 203);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(100, 22);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(171, 203);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "button1";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // ckbAudio
+            // 
+            this.ckbAudio.AutoSize = true;
+            this.ckbAudio.Location = new System.Drawing.Point(171, 274);
+            this.ckbAudio.Name = "ckbAudio";
+            this.ckbAudio.Size = new System.Drawing.Size(110, 21);
+            this.ckbAudio.TabIndex = 2;
+            this.ckbAudio.TabStop = true;
+            this.ckbAudio.Text = "radioButton1";
+            this.ckbAudio.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label1";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(493, 321);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(370, 171);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(731, 417);
+            this.ClientSize = new System.Drawing.Size(875, 504);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ckbAudio);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.RadioButton ckbAudio;
+        private System.Windows.Forms.Label label4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
--- ProyectoFinal3/Form1.cs
+++ ProyectoFinal3/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NReco.VideoConverter;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
 
 namespace ProyectoFinal3
 {
@@ -15,6 +19,72 @@ namespace ProyectoFinal3
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private async Task MainAsync()
+        {
+            //Nuevo Cliente de YouTube
+            var client = new YoutubeClient();
+            //Lee la URL de youtube que le escribimos en el textbox.
+            var videoId = NormalizeVideoId(txtURL.Text);
+            var video = await client.GetVideoAsync(videoId);
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(videoId);
+            //Busca la mejor resolución en la que está disponible el video.
+            var streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
+            //Compone el nombre que tendrá el video en base a su título y extensión.
+            var fileExtension = streamInfo.Container.GetFileExtension();
+            var fileName = $"{video.Title}.{fileExtension}";
+            //TODO: Reemplazar los caractéres ilegales del nombre
+            //fileName = RemoveIllegalFileNameChars(fileName);
+            //Activa el timer para que el proceso funcione de forma asincrona
+            ckbAudio.Enabled = true;
+            // mensajes indicando que el video se está descargando
+            label4.Text = "Descargando el video...";
+            //TODO: se pude usar una barra de progreso para ver el avance
+            //using (var progress = new ProgressBar());
+            //Empieza la descarga.
+            await client.DownloadMediaStreamAsync(streamInfo, fileName);
+            //Ya descargado se inicia la conversión a MP3
+            var Convert = new NReco.VideoConverter.FFMpegConverter();
+            //Especificar la carpeta donde se van a guardar los archivos, recordar la \ del final
+            String SaveMP3File = @"C:\Users\Carlos Escobar\Source\Repos\programacion\Pro3Play\MP3\" + fileName.Replace(".mp4", ".mp3");
+            //Guarda el archivo convertido en la ubicación indicada
+            Convert.ConvertMedia(fileName, SaveMP3File, "mp3");
+            //Si el checkbox de solo audio está chequeado, borrar el mp4 despues de la conversión
+            if (ckbAudio.Checked)
+            {
+                File.Delete(fileName);
+            }
+            //Indicar que se terminó la conversion
+            MessageBox.Show("Vídeo convertido correctamente.");
+            label4.Text = "";
+            txtURL.Text = "";
+            ckbAudio.Enabled = false;
+            //TODO: Cargar el MP3 al reproductor o a la lista de reproducción
+            //CargarMP3s();
+            //Se puede incluir un checkbox para indicar que de una vez se reproduzca el MP3
+            //if (ckbAutoPlay.Checked) 
+            //  ReproducirMP3(SaveMP3File);
+            return;
+        }
+
+        private static string NormalizeVideoId(string input)
+        {
+            string videoId = string.Empty;
+            return YoutubeClient.TryParseVideoId(input, out videoId)
+                ? videoId
+                : input;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            MainAsync();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
